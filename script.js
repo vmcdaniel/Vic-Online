82bbefa170e42c14c36674405cdb9b28ddec5c2d
Victoria <vmcdaniel459@gmail.com>
2024-06-28 18:00:17 -0400

    basic HTML structure and wording

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//const h1 = document.createElement('h1');
+h1.textContent = 'Welcome to Vic Online'
+document.querySelector(head).appendChild(h1);
+
+
 const h2 = document.createElement('h2');
 h2.textContent = 'This content added by JavaScript';
 document.querySelector('body').appendChild(h2);
+const title = document.createElement('title')
